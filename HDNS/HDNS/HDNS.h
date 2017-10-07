ef498fb4a47f304d9a5bdfc82e1fe3b6da823313
冯勇海 <2206728426@qq.com>
2017-10-07 16:52:04 +0800

    支持carthage

---
+++ HDNS/HDNS/HDNS.h
@@ -0,0 +1,30 @@
+//
+//  HDNS.h
+//  HDNS
+//
+//  Created by 冯勇海 on 2017/10/7.
+//  Copyright © 2017年 Chai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDNS.
+FOUNDATION_EXPORT double HDNSVersionNumber;
+
+//! Project version string for HDNS.
+FOUNDATION_EXPORT const unsigned char HDNSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDNS/PublicHeader.h>
+
+#import <HDNS/QNDnsManager.h>
+#import <HDNS/QNDnspodEnterprise.h>
+#import <HDNS/QNDnspodFree.h>
+#import <HDNS/QNDomain.h>
+#import <HDNS/QNHijackingDetectWrapper.h>
+#import <HDNS/QNIP.h>
+#import <HDNS/QNNetworkInfo.h>
+#import <HDNS/QNRecord.h>
+#import <HDNS/QNResolver.h>
+#import <HDNS/QNResolverDelegate.h>
+
+#import <HDNS/QNGetAddrInfo.h>
